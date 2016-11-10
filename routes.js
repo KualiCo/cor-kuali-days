68f7c21b8b5453649db4005b8500cb92b9a041a8
Michael Graybeal <michael@thegraybeals.com>
2016-11-09 19:08:25 -0700

    removing uneeded logs

--- routes.js
+++ routes.js
@@ -9,17 +9,11 @@ const routes = new express.Router()
 routes.post('/api/login', (req, res) => {
   kuali.authenticate(req.get('Authorization'))
   .then(token => {
-    console.log('token', token)
     req.session.token = token
-    console.log('req.session', req.session)
-    console.log('req.session.id', req.session.id)
-    req.session.save()
     res.end()
   })
 })
 routes.get('/api/current-user', (req, res, next) => {
-  console.log('req.session.id', req.session.id)
-  console.log('req.session', req.session)
   kuali.getCurrentUser(req.session.token)
   .then(user => {
     res.send(user)
