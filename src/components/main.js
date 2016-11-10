f912eae446e05653e151aabed6e75c3056fca045
Michael Graybeal <michael@thegraybeals.com>
2016-11-10 12:44:40 -0700

    removing user stuff from ui

--- src/components/main.js
+++ src/components/main.js
@@ -11,12 +11,7 @@ class Main extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      userList: [],
-      selectedUsers: {}
     }
-    this.rowSelection = this.rowSelection.bind(this)
-    this.setUserList = this.setUserList.bind(this)
-    this.approve = this.approve.bind(this)
   }
 
   componentWillMount() {
@@ -25,55 +20,9 @@ class Main extends Component {
         user: res.data
       })
     })
-    .then(this.setUserList)
     .catch(() => {
       this.context.router.push('/login')
     })
-  }
-
-  setUserList() {
-    return axios.get('/api/unapproved-users')
-    .then(res => {
-      this.setState({
-        userList: res.data
-      })
-    })
-  }
-
-  rowSelection(row) {
-    let selectedUsers
-    if (row === 'all') {
-      selectedUsers = this.state.userList.reduce((list, u) => {
-        return {
-          ...list,
-          [u.id]: true
-        }
-      }, {})
-    } else if (row === 'none') {
-      selectedUsers = {}
-    } else if (row instanceof Array) {
-      selectedUsers = row.reduce((list, i) => {
-        return {
-          ...list,
-          [this.state.userList[i].id]: true
-        }
-      }, {})
-    }
-    this.setState({
-      selectedUsers
-    })
-  }
-
-  approve() {
-    const selectedUsers = this.state.userList.reduce((list, u) => {
-      if (this.state.selectedUsers[u.id]){
-        list.push(u)
-      }
-      return list
-    }, [])
-    axios.post('/api/approve-users',
-      selectedUsers
-    ).then(this.setUserList)
   }
 
   render() {
@@ -83,37 +32,7 @@ class Main extends Component {
           <div>
             <div className="header">
               <span className="current-user">Logged in as: {this.state.user.displayName}</span>
-              <h3>Unapproved Users</h3>
             </div>
-            <Table multiSelectable={true}
-              onRowSelection={this.rowSelection}
-            >
-              <TableHeader enableSelectAll={true}>
-                <TableRow>
-                  <TableHeaderColumn>Username</TableHeaderColumn>
-                  <TableHeaderColumn>Email</TableHeaderColumn>
-                  <TableHeaderColumn>Name</TableHeaderColumn>
-                  <TableHeaderColumn>Created At</TableHeaderColumn>
-                </TableRow>
-              </TableHeader>
-              <TableBody deselectOnClickaway={false}>
-                {this.state.userList.map(user => (
-                  <TableRow key={user.id} selected={this.state.selectedUsers[user.id]}>
-                    <TableRowColumn>{user.username}</TableRowColumn>
-                    <TableRowColumn>{user.email}</TableRowColumn>
-                    <TableRowColumn>{user.displayName}</TableRowColumn>
-                    <TableRowColumn>{new Date(user.createdAt).toString()}</TableRowColumn>
-                  </TableRow>))
-                }
-              </TableBody>
-            </Table>
-            <RaisedButton
-              className="approve"
-              label="Approve"
-              primary={true}
-              onClick={this.approve}
-            />
-          </div>
         )}
       </Paper>
     )
